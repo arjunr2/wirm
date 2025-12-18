4b2b55886375825edc5183a8b33372d2c8d3ac18
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2025-12-18 18:44:02 -0500

    finish implementing the ASSIGN phase

--- src/encode/component/assign.rs
+++ src/encode/component/assign.rs
@@ -85,25 +85,12 @@ use crate::ir::section::ComponentSection;
 pub(crate) fn assign_indices<'a>(plan: &mut ComponentPlan<'a>, indices: &mut IdxSpaces) {
     for item in &mut plan.items {
         match item {
-            ComponentItem::Component{ node, plan: subplan, idx, ..} => {
-                // indices.reset_ids();
-                // let ptr = *node as *const _;
-                // if !indices.component.contains_key(&ptr) {
-                //     // I've not visited this node yet!
-                //
-                //     // Visit this component's internals
-                //     let (sub_indices, sub_spaces) = assign_indices(subplan, idx_spaces);
-                //
-                //     // Assign the component an ID and remember what it was originally!
-                //     // This allows us to fix ID mappings at encode time.
-                //     indices.component.insert(ptr, next_comp);
-                //     // spaces.comp.insert(*original_id, next_comp);
-                //     next_comp += 1;
-                //
-                //     // Save the metadata in the ComponentItem itself!
-                //     item.update_comp_metadata(sub_indices, sub_spaces);
-                // }
-                todo!()
+            ComponentItem::Component{ plan: subplan, indices: subindices, idx, .. } => {
+                // Visit this component's internals
+                indices.reset_ids();
+                assign_indices(subplan, subindices);
+
+                indices.assign_actual_id(&ComponentSection::Component, &ExternalItemKind::NA, *idx);
             }
             ComponentItem::Module { idx, .. } => {
                 indices.assign_actual_id(&ComponentSection::Module, &ExternalItemKind::NA, *idx);
@@ -145,7 +132,7 @@ pub(crate) fn assign_indices<'a>(plan: &mut ComponentPlan<'a>, indices: &mut Idx
                 indices.assign_actual_id(&ComponentSection::CoreInstance, &ExternalItemKind::NA, *idx);
             }
             ComponentItem::CustomSection { .. } => {
-                todo!()
+                // NA: Custom sections don't get IDs
             }
         }
     }
